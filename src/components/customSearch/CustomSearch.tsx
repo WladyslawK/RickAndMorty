beeba9b85a83248af2baea5dff20b4bf1fbd3ae4
WladyslawK <vladkurr7890@gmail.com>
2023-01-26 23:35:36 +0100

    feat(CustomSearch) add CustomSearch

--- src/components/customSearch/CustomSearch.tsx
+++ src/components/customSearch/CustomSearch.tsx
@@ -1,80 +1,140 @@
-import React, {ChangeEvent, useState} from 'react';
-import {FormControl, IconButton, InputAdornment, InputLabel, MenuItem, OutlinedInput, TextField} from "@mui/material";
+import React, {ChangeEvent, createRef, useRef, useState} from 'react';
+import {
+  FormControl,
+  IconButton,
+  InputAdornment, InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+  TextField
+} from "@mui/material";
 import s from './CustomSerch.module.css'
-import {FilterAltOff, Search, Visibility, VisibilityOff} from "@mui/icons-material";
+import {FilterAltOff, Search} from "@mui/icons-material";
 import {speciesFilter, status} from "../../constants/constants";
+import {useAppDispatch} from "../../hooks/reduxHooks";
+import {initialCharactersState, updateQueryParams} from "../../features/allCharactersPage/charactersSlice";
 
 export const CustomSearch = () => {
 
-  const [inputValue, setInputValue] = useState('')
+  const dispatch = useAppDispatch()
+  const [queryName, setQueryName] = useState('')
+  const [queryStatus, setQueryStatus] = useState('')
+  const [querySpecies, setQuerySpecies] = useState('')
 
-  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.currentTarget.value)
+  const changeStatusHandler = (event: SelectChangeEvent<string>) => {
+    const status = event.target.value
+    setQueryStatus(status)
+    dispatch(updateQueryParams({page: '1', status}))
   }
 
-  const [showPassword, setShowPassword] = React.useState(false);
+  const changeSpeciesHandler = (event: SelectChangeEvent<string>) => {
+    const species = event.target.value
+    setQuerySpecies(species)
+    dispatch(updateQueryParams({page: '1', species}))
+  }
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
+  const inputRef: any = useRef(null)
+  const onChangeNameHandler = (name: string) => {
+
+    console.log('hello from debounce1: ', name[0])
+    //setQueryName(name)
+    dispatch(updateQueryParams({page: '1', name}))
+  }
+
+  function debounce(callback: (e: any) => void, delay: number = 1000) {
+    let timeout: any
+
+    return (...args: any) => {
+      clearTimeout(timeout)
+
+      timeout = setTimeout(() => {
+        callback(args)
+      }, delay)
+    }
+  }
+
+  console.log("STATUS: ", queryStatus)
+
+  const resetAllFiltersHandler = () => {
+    dispatch(updateQueryParams({...initialCharactersState.queryParams}))
+    inputRef.current = ''
+    setQueryStatus('')
+    setQuerySpecies('')
+  }
+
+  const debouncedChange = debounce(onChangeNameHandler, 1000)
 
   return (
-    <div className={s.searchContainer}>
-      {/*<TextField className={s.input} id="outlined-basic" variant="outlined" size='small' />*/}
-      <FormControl sx={{ width: '25ch' }} className={s.nameInput} variant="outlined" size='small'>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          type={showPassword ? 'text' : 'password'}
-          startAdornment={
-            <InputAdornment position="start">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                <Search/>
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-      </FormControl>
+    <div className={s.mainContainer}>
+      <div className={s.searchContainer}>
+        {/*<TextField className={s.input} id="outlined-basic" variant="outlined" size='small' />*/}
+        <FormControl sx={{width: '25ch'}} className={s.nameInput} variant="outlined" size='small'>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type='text'
+            startAdornment={
+              <InputAdornment position="start">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  edge="end"
+                >
+                  <Search/>
+                </IconButton>
+              </InputAdornment>
+            }
+            onChange={(e) => debouncedChange(e.currentTarget.value)}
+            ref={inputRef}
+          />
+        </FormControl>
 
-      <TextField
-        id="outlined-select-currency"
-        select
-        label="status"
-        defaultValue=""
-        className={s.statusInput}
-        size='small'
-      >
-        {status.map((option, i) => (
-          <MenuItem key={i} value={option}>
-            {option}
-          </MenuItem>
-        ))}
-      </TextField>
+        <FormControl>
+          <InputLabel id="demo-simple-select-label" >Status</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            name={'status'}
+            label="status"
+            className={s.statusInput}
+            onChange={changeStatusHandler}
+            value={queryStatus}
+            size='small'
+          >
+            {status.map((option, i) => (
+              <MenuItem key={i} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <TextField
-        id="outlined-select-currency"
-        select
-        label="species"
-        defaultValue=""
-        className={s.speciesInput}
-        size='small'
-      >
-        {speciesFilter.map((option, i) => (
-          <MenuItem key={i} value={option}>
-            {option}
-          </MenuItem>
-        ))}
-      </TextField>
+        <FormControl>
+          <InputLabel id="demo-simple-select-label" >Species</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            name={'species'}
+            label="species"
+            className={s.speciesInput}
+            onChange={changeSpeciesHandler}
+            value={querySpecies}
+            size='small'
+          >
+            {speciesFilter.map((option, i) => (
+              <MenuItem key={i} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <div className={s.resetAllFilters}>
-        <FilterAltOff/>
+
+        <div className={s.resetAllFilters} onClick={resetAllFiltersHandler}>
+          <FilterAltOff/>
+        </div>
       </div>
     </div>
+
   )
 }
