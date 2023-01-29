99d2dbdf57f541397255b2a4d8cd797aa0fc02a5
WladyslawK <vladkurr7890@gmail.com>
2023-01-29 03:36:04 +0100

    feat(AppSlice tests) AppSlice tests added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
 import {Navbar} from "../components/navbar/Navbar"
-import {Navigate, Route, Routes} from "react-router-dom";
-import {PATH} from "../constants/routePaths.enum";
-import {AllCharactersPage} from "../features/allCharactersPage/AllCharactersPage";
-import {CharacterInformation} from "../features/characterInformation/CharacterInformation";
+import {Navigate, Route, Routes} from "react-router-dom"
+import {PATH} from "../constants/routePaths.enum"
+import {AllCharactersPage} from "../features/allCharactersPage/AllCharactersPage"
+import {CharacterInformation} from "../features/characterInformation/CharacterInformation"
+import {ErrorSnackBar} from "../components/errorSnackBar/ErrorSnackBar"
 
 export const App = () => {
 
@@ -18,6 +19,7 @@ export const App = () => {
           <Route path={PATH.CHARACTER_INFO_CHARACTER_ID} element={<CharacterInformation/>}/>
         </Route>
       </Routes>
+      <ErrorSnackBar />
     </>
   )
 }
